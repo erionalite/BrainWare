5befbf8e36ae8eb816eac4a6c7a6c9764f04f82d
Erion Alite <erion.alite@gmail.com>
2018-10-23 23:24:51 -0400

    adding tests.

---
+++ Tests/ServicesTest/CompanyRespositoryTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using Web.Infrastructure;
+using Web.Models;
+using Web.Repositories;
+
+namespace Tests.ServicesTest
+{
+
+    [TestClass]
+    public class CompanyRespositoryTest
+    {
+        [TestMethod]
+        public void FindCompany_expectNotEmpty()
+        {
+            var mockOrderContext = MockRepository.GenerateStub<IOrderContext>();
+            
+            mockOrderContext.Stub(x => x.Companies).Return(new CompanyEntity[] { new CompanyEntity()
+            {
+                company_id = 0,
+                Name= "test"
+            }});
+            var companyRespository = new CompanyRepository(mockOrderContext);
+            var companyEntity = companyRespository.GetCompany(0);
+            Assert.IsTrue(companyEntity.Name == "test");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Could not find company with id :0")]
+        public void FindCompany_expectNull()
+        {
+            var mockOrderContext = MockRepository.GenerateStub<IOrderContext>();
+            mockOrderContext.Stub(x => x.Companies).Return(new CompanyEntity[] { new CompanyEntity()
+            {
+                company_id = 1,
+                Name= "test"
+            }});
+            var companyRespository = new CompanyRepository(mockOrderContext);
+            var companyEntity = companyRespository.GetCompany(0);
+        }
+    }
+}
---
+++ Tests/ServicesTest/OrderServiceTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Infrastructure;
+using Web.Models;
+using Web.Repositories;
+
+namespace Tests.ServicesTest
+{
+    [TestClass]
+    public class OrderServiceTest
+    {
+        [TestMethod]
+        public void FindCompany_expectEmpty()
+        {
+            var mockCompanyRepository = MockRepository.GenerateStub<ICompanyRepository>();
+            mockCompanyRepository.Stub(x => x.GetCompany(Arg<int>.Is.Anything)).Return(null);
+            var orderService = new OrderService(mockCompanyRepository);
+            var result = orderService.GetOrdersForCompany(0);
+            Assert.IsTrue(!result.Any());
+        }
+        [TestMethod]
+        public void FindCompany_expectNotEmpty()
+        {
+            var mockCompanyRepository = MockRepository.GenerateStub<ICompanyRepository>();
+            mockCompanyRepository.Stub(x => x.GetCompany(Arg<int>.Is.Anything)).Return(new CompanyEntity()
+            {
+                company_id = 0,
+                Name = "test",
+                Orders = new List<OrderEntity>()
+                {
+                    new OrderEntity()
+                    {
+                        CompanyEntity = new CompanyEntity()
+                        {
+                            company_id = 1,
+                            Name = "testCompany"
+                        },
+                        CompanyId = 1,
+                        Description = "testDescription",
+                        Order_Id = 2,
+                        OrderProducts = new List<OrderProductEntity>()
+                        {
+                            new OrderProductEntity()
+                            {
+                                OrderId = 2,
+                                Price = 22,
+                                Quantity = 1,
+                                Product = new ProductEntity()
+                                {
+                                    Name = "Test Product",
+                                    Price = 33
+                                }
+                            }
+                        }
+
+
+                    }
+                }
+            });
+            var orderService = new OrderService(mockCompanyRepository);
+            var result = orderService.GetOrdersForCompany(0);
+            Assert.IsTrue(result.Any());
+        }
+
+    }
+}
